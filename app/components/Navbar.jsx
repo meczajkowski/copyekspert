99afc6f10125bc7e1188907138b507f6ca96b5e3
Michal <michal.erik.czajkowski@gmail.com>
2023-09-05 23:00:46 +0200

    feat: create navbar - add logo and base style

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,16 +2,49 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href='/'>Strona główna</Link>
-      <Link href='/oferta'>Oferta</Link>
-      <Link href='/serwis'>Serwis</Link>
-      <Link href='https://www.konicaminolta.pl/pl-pl/support/centrum-pobierania'>
-        Sterowniki
+    <nav class='h-16 mx-auto px-5 flex items-center justify-between'>
+      <Link href='https://copyekspert.pl/' className='flex items-center'>
+        <img
+          src='copyekspert_logo.gif'
+          className='h-8 mr-3'
+          alt='Copyekspert Logo'
+        />
       </Link>
-      <Link href='/kariera'>Kariera</Link>
-      <Link href='/blog'>Blog</Link>
-      <Link href='/kontakt'>Kontakt</Link>
+
+      <ul class='flex items-center gap-5'>
+        <li>
+          <Link
+            href='/'
+            className='hover:text-primary transition-colors'
+            aria-current='page'
+          >
+            Strona główna
+          </Link>
+        </li>
+        <li>
+          <Link href='/oferta' className='hover:text-primary transition-colors'>
+            Oferta
+          </Link>
+        </li>
+        <li>
+          <Link href='/serwis' className='hover:text-primary transition-colors'>
+            Serwis
+          </Link>
+        </li>
+        <li>
+          <Link
+            href='https://www.konicaminolta.pl/pl-pl/support/centrum-pobierania'
+            className='hover:text-primary transition-colors'
+          >
+            Sterowniki
+          </Link>
+        </li>
+        <li>
+          <a href='/kontakt' className='hover:text-primary transition-colors'>
+            Kontakt
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        text: '#060505',
+        background: '#ffffff',
+        primary: '#1e88e5',
+        secondary: '#eeeeee',
+        accent: '#1e88e5',
       },
     },
   },
