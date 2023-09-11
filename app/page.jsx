d860695e3c2ef8f70ca8797ba396962da68ddc6e
Michal <michal.erik.czajkowski@gmail.com>
2023-09-12 00:44:12 +0200

    feat: create hero section - add elements

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 
+// components
+import HeroSection from '@/components/HeroSection/HeroSection';
+
 export default function Home() {
   return (
     <main>
-      <h1>Urządzenia Wielofunkcyjne Konica Minolta we Wrocławiu</h1>
+      <HeroSection />
     </main>
   );
 }
---
+++ components/HeroSection/HeroSection.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import styles from './HeroSection.module.css';
+
+export default function HeroSection() {
+  return (
+    <section className={styles.heroSection}>
+      <div className={styles.header}>
+        <h1 className={styles.heading}>
+          Urządzenia Wielofunkcyjne Konica Minolta we Wrocławiu
+        </h1>
+        <p className={styles.desc}>
+          Jesteśmy ekspertami w dziedzinie biurowych urządzeń drukujących
+        </p>
+        <button className={styles.ctaButton}>Oferta</button>
+      </div>
+      <div className={styles.heroImgContainer}>
+        <Image
+          className={styles.heroImg}
+          width={837}
+          height={670}
+          src='/hero-img.webp'
+          alt='hero image'
+        />
+      </div>
+    </section>
+  );
+}
