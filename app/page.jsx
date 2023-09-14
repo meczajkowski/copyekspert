3077d10f0ce317302982b8409989fdc3b69fc989
Michal <michal.erik.czajkowski@gmail.com>
2023-09-14 23:19:06 +0200

    feat: create features section - install tailwind css

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,13 @@ import styles from './page.module.css';
 
 // components
 import HeroSection from '@/components/HeroSection/HeroSection';
+import FeaturesSection from '@/components/FeaturesSection/FeaturesSection';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <FeaturesSection />
     </main>
   );
 }
---
+++ components/FeaturesSection/FeaturesSection.jsx
@@ -0,0 +1,3 @@
+export default function FeaturesSection() {
+  return <div className='text-orange-500'>FeaturesSection</div>;
+}
