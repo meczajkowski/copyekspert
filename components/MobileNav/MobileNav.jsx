8525a3e075bf348ce973dd8c34696e64e2632781
Michal <michal.erik.czajkowski@gmail.com>
2023-09-17 12:06:18 +0200

    change: switching to tailwind - mobile nav with shadcn - close on click

--- components/MobileNav/MobileNav.jsx
+++ components/MobileNav/MobileNav.jsx
@@ -1,5 +1,10 @@
 import { Menu } from 'lucide-react';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetClose,
+} from '@/components/ui/sheet';
 import { routes } from '../Navbar/Navbar';
 import Link from 'next/link';
 
@@ -15,14 +20,16 @@ const MobileNav = (props) => {
             <ul className={'py-6 px-4'}>
               {routes.map((route, index) => (
                 <li key={index}>
-                  <Link
-                    className={
-                      'block px-4 py-4 text-center rounded-lg transition-all hover:bg-primary hover:text-background hover:scale-110 hover:shadow-lg'
-                    }
-                    href={route.href}
-                  >
-                    {route.label}
-                  </Link>
+                  <SheetClose asChild>
+                    <Link
+                      className={
+                        'block px-4 py-4 text-center rounded-lg transition-all hover:bg-primary hover:text-background hover:scale-110 hover:shadow-lg'
+                      }
+                      href={route.href}
+                    >
+                      {route.label}
+                    </Link>
+                  </SheetClose>
                 </li>
               ))}
             </ul>
