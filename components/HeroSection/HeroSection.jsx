3336af0e9dbcebd32d0aa2419be7dbf8fb2e572f
Michal <michal.erik.czajkowski@gmail.com>
2023-09-20 19:11:54 +0200

    feat: integrate shadcn into project - hero buttons

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Container from '../ui/Container';
 import heroImg from '/public/hero-img.png';
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
 
 const HeroSection = () => {
   return (
@@ -20,32 +22,24 @@ const HeroSection = () => {
             </p>
             {/* Buttons */}
             <div className='mt-7 grid gap-3 w-full sm:inline-flex'>
-              <a
-                className='inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-sm lg:text-base text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800'
+              <Link
+                className={`${buttonVariants({
+                  variant: 'default',
+                  size: 'lg',
+                })} `}
                 href='#'
               >
                 Dowiedz się więcej
-                <svg
-                  className='w-2.5 h-2.5'
-                  width={16}
-                  height={16}
-                  viewBox='0 0 16 16'
-                  fill='none'
-                >
-                  <path
-                    d='M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14'
-                    stroke='currentColor'
-                    strokeWidth={2}
-                    strokeLinecap='round'
-                  />
-                </svg>
-              </a>
-              <a
-                className='inline-flex justify-center items-center gap-x-3.5 text-sm lg:text-base text-center border hover:border-gray-300 shadow-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:border-gray-800 dark:hover:border-gray-600 dark:shadow-slate-700/[.7] dark:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800'
+              </Link>
+              <Link
+                className={`${buttonVariants({
+                  variant: 'outline',
+                  size: 'lg',
+                })} `}
                 href='#'
               >
                 Skontaktuj się z działem sprzedaży
-              </a>
+              </Link>
             </div>
             {/* End Buttons */}
             {/* Review */}
